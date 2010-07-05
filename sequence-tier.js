a014f65d4ac49b71c9c88ace293a11bbe908ffa4
Thomas Down <thomas.a.down@googlemail.com>
2010-07-05 22:23:01 +0100

    Initial crude job of factoring out the tier-renderers.
    Next step: make the calling conventions somewhat sane!

---
+++ sequence-tier.js
@@ -0,0 +1,98 @@
+// 
+// Dalliance Genome Explorer
+// (c) Thomas Down 2006-2010
+//
+// sequence-tier.js: renderers for sequence-related data
+//
+
+var rulerTileColors = ['black', 'white'];
+var baseColors = {A: 'green', C: 'blue', G: 'black', T: 'red'};
+
+function drawSeqTier(tier, seq)
+{
+    var featureGroupElement = tier.viewport;
+    while (featureGroupElement.childNodes.length > 0) {
+	    featureGroupElement.removeChild(featureGroupElement.firstChild);
+	}
+	featureGroupElement.appendChild(tier.background);
+    
+	var steps = [1,2,5];
+	function ts(p) {
+	    return steps[p % steps.length] * Math.pow(10, (p / steps.length)|0);
+	}
+	var pow = steps.length;
+	while (scale * ts(pow) < 50) {
+	    ++pow;
+	}
+	var tile = ts(pow);
+	var pos = Math.max(0, ((knownStart / tile)|0) * tile);
+	
+	var seqTierMax = knownEnd;
+	if (currentSeqMax > 0 && currentSeqMax < knownEnd) {
+	    seqTierMax = currentSeqMax;
+	}
+	
+	while (pos <= seqTierMax) {
+        var rect = document.createElementNS(NS_SVG, "rect");
+        rect.setAttribute('x', (pos - origin) * scale);
+        rect.setAttribute('y', 5);
+        rect.setAttribute('height', 3);
+        var rwid = Math.min(tile, seqTierMax - pos) * scale;
+        rect.setAttribute('width', rwid);
+        rect.setAttribute('fill', rulerTileColors[(pos / tile) % 2]);
+        rect.setAttribute('stroke-width', 1);
+        featureGroupElement.appendChild(rect);
+        
+        if ((pos / tile) % 2 == 0) {
+            var labelText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+            labelText.setAttribute("x", ((pos - origin) * scale));
+            labelText.setAttribute("y",  25);
+            labelText.setAttribute("stroke-width", "0");
+            labelText.setAttribute("fill", "black");
+            labelText.setAttribute("class", "label-text");
+            labelText.appendChild(document.createTextNode('' + pos));
+            featureGroupElement.appendChild(labelText);
+        }
+	     
+	    pos += tile;
+	}
+	
+	var height = 30;
+	
+	if (seq) {
+	    for (var i = seq.start; i <= seq.end; ++i) {
+	        var base = seq.seq.substr(i - seq.start, 1).toUpperCase();
+	        var color = baseColors[base];
+	        if (!color) {
+	            color = 'gray';
+	        }
+	        
+	        if (scale >= 8) {
+                var labelText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                labelText.setAttribute("x", ((i - origin) * scale));
+                labelText.setAttribute("y",  45);
+                labelText.setAttribute("stroke-width", "0");
+                labelText.setAttribute("fill", color);
+                labelText.setAttribute("class", "label-text");
+                labelText.appendChild(document.createTextNode(base));
+                featureGroupElement.appendChild(labelText);
+                height = 50;
+            } else {
+                var rect = document.createElementNS(NS_SVG, "rect");
+                rect.setAttribute('x', ((i - origin) * scale));
+                rect.setAttribute('y', 30);
+                rect.setAttribute('height', 8);
+                rect.setAttribute('width', scale);
+                rect.setAttribute('fill', color);
+                rect.setAttribute('stroke', 'none');
+                featureGroupElement.appendChild(rect);
+                height = 42;
+            }
+        }
+    }
+	
+    tier.layoutHeight = height;
+    tier.background.setAttribute("height", height);
+    tier.scale = 1;
+    arrangeTiers();
+}
