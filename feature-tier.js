81d9d31bbf22f750d558282b581b36fc612fe37e
Thomas Down <thomas.a.down@googlemail.com>
2010-07-11 13:29:12 +0100

    Ported gene popup code (is this general enough?)

--- feature-tier.js
+++ feature-tier.js
@@ -206,7 +206,7 @@ function drawFeatureTier(tier)
     });
     for (var gx in gl) {
 	var gid = gl[gx];
-	var g = glyphsForGroup(tier.groupedFeatures[gid], offset, styles);
+	var g = glyphsForGroup(tier.groupedFeatures[gid], offset, styles, tier.groups[gid]);
 	glyphs.push(g);
     }
 
@@ -256,7 +256,7 @@ function drawFeatureTier(tier)
     tier.scale = 1;
 }
 
-function glyphsForGroup(features, y, stylesheet) {
+function glyphsForGroup(features, y, stylesheet, groupElement) {
     var height=1;
     var label;
     var spans = null;
@@ -306,6 +306,50 @@ function glyphsForGroup(features, y, stylesheet) {
 	}
 	    
 	glyphGroup.appendChild(path);
+    }
+
+    //
+    // popup code ported from old renderer.
+    // FIXME: is this really the place?
+    //
+    if (groupElement && groupElement.links) {
+	glyphGroup.addEventListener('mouseover', function(ev) {
+	    var link = '';
+	    for (var li = 0; li < groupElement.links.length; ++li) {
+	        var dasLink = groupElement.links[li];
+	        link += ' <a href="' + dasLink.uri + '">(' + dasLink.desc + ')</a>';
+	    }
+	    	            
+	    removeAllPopups();
+	    var mx =  ev.clientX, my = ev.clientY;
+	    mx +=  document.documentElement.scrollLeft || document.body.scrollLeft;
+	    my +=  document.documentElement.scrollTop || document.body.scrollTop;
+	    var popup = $('#popupTest').clone().css({
+	        position: 'absolute', 
+	        top: (my - 10), 
+	        left:  (mx - 10),
+	        width: 200,
+	        backgroundColor: 'white',
+	        borderColor: 'black',
+	        borderWidth: 1,
+	        borderStyle: 'solid',
+	        padding: 2,
+	    }).html('Gene: ' + label + link).get(0);
+	    $(popup).hide();
+	    hPopupHolder.appendChild(popup);
+	    $(popup).fadeIn(500);
+	            
+	    popup.addEventListener('mouseout', function(ev2) {
+	        var rel = ev2.relatedTarget;
+	        while (rel) {
+	            if (rel == popup) {
+	                return;
+	            }
+	            rel = rel.parentNode;
+	        }
+	        removeAllPopups();
+	    }, false);
+	}, true);
     }
 
     var dg = new DGlyph(glyphGroup, spans.min(), spans.max(), height);
@@ -577,186 +621,4 @@ function labelGlyph(dglyph) {
 	dglyph.height = dglyph.height + 20;
     }
     return dglyph;
-}
-
-function drawFeatureGroup(featureGroupElement, y, features, bumpMatrix, label, renderer, groupElement)
-{
-    var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    
-    var gmin = 10000000000;
-    var gmax = -10000000000;
-    var blockList = new Array(0);
-    var strand = null;
-    var elideTents = false;
-    var th = renderer.height();
-    
-    for (var i = 0; i < features.length; ++i) {
-        var feature = features[i];
-        var min = feature.min;
-        var max = feature.max;
-        var type = feature.type;
-        if (strand == null && feature.orientation) {
-            strand = feature.orientation;
-        }
-        var score = feature.score;
-        if (label != null && label != "") {
-            if (feature.label) {
-                label = feature.label;
-            }
-        }
-        
-        if (type == 'density') {
-           bumpMatrix = null;      // HAXXXX!
-        }
-
-        var minPos = (min - origin) * scale;
-        var maxPos = (max - origin) * scale;
-        gmin = Math.min(minPos, gmin);
-        gmax = Math.max(maxPos, gmax);
-
-	var fMinSize = 5.0;       
-        if (maxPos - minPos < fMinSize) {
-            var midPos = (maxPos + minPos) / 2;
-            minPos = midPos - (fMinSize/2);
-            maxPos = minPos + (fMinSize/2);
-        }
- 
-        var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        rect.setAttribute("x", minPos);
-        rect.setAttribute("y", y);
-        rect.setAttribute("width", maxPos - minPos);
-	if (th >= 18) {
-            rect.setAttribute("height", "12");
-        } else {
-	    rect.setAttribute("height", th - 4);
-        }
-        var fill="blue", stroke="1"
-        if (type == "translation") {
-		  	fill="red";
-		} else {
-			if (type == "exon" || type == 'transcript') {
-		  	    fill="cornsilk";
-                        } else if (type == "cpg-meth") {
-                            fill="blue"; stroke="0";
-                        } else if (type == "cpg-unmeth") {
-                            fill="red"; stroke="0";
-                        } else if (type == "read") {
-                            fill="cornsilk"; elideTents = true;
-			} else if (type == "meth" || type == "depth") {
-			     if (BoxRenderer.prototype.isPrototypeOf(renderer)) {
-			        var red = 0,green = 0,blue = 255;
-			        if (score < 50) {
-			            var prop = Math.max(0, score - 20) / 30;
-			            green = 255;
-			            red = (1.0 - prop) * 255;
-			            blue = 0;
-			        } else {
-			            var prop = Math.min(30, score - 50) / 30
-			            red = 0;
-			            green = (1.0 - prop) * 255;
-			            blue = prop * 255;
-			        }
-			        fill = "rgb(" + (red|0) + "," + (green|0) + "," + (blue|0) +")";
-			        stroke = "0";
-			        rect.setAttribute("width", maxPos - minPos + 1);
-			    } 
-			    
-			}  else {
-			    var soc = styleOracle[type];
-			    if (soc) {
-				fill = soc;
-			    }
-			}
-			blockList.push(new Range(minPos, maxPos));
-			if (strand == "-") {
-				isReverseStrand = true;
-			}
-		}
-		if (BarRenderer.prototype.isPrototypeOf(renderer) /* || type == 'density' */) {
-		    var sc = (score * renderer.scale)|0;
-			rect.setAttribute("y", y + renderer.height() - sc);
-			rect.setAttribute("height", sc);
-		} else if (type == 'density') {
-		    var sc = score | 0;
-		    rect.setAttribute("y", y + renderer.height() - sc);
-		    rect.setAttribute("height", sc);
-		}
-		rect.setAttribute("fill", fill);
-		rect.setAttribute("stroke-width", stroke);
-		g.appendChild(rect);
-	}
-
-
-	if (label != null && label != "" && type != 'read' && type != 'cpg-meth' && type != 'cpg-unmeth') {
-	    var labelText = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	    labelText.setAttribute("x", gmin);
-	    labelText.setAttribute("y", y + 25);
-	    labelText.setAttribute("stroke-width", "0");
-	    labelText.setAttribute("fill", "black");
-	    labelText.setAttribute("class", "label-text");
-	    var lt = label;
-	    if (strand == '+') {
-		lt = label + '>';
-	    } else if (strand == '-') {
-		lt = '<' + label;
-            }
-	    labelText.appendChild(document.createTextNode(lt));
-	    g.appendChild(labelText);
-	    
-	    g.addEventListener("mouseover", function(ev) {
-	            var link = '';
-	            if (groupElement) {
-	                if (groupElement.links) {
-	                    for (var li = 0; li < groupElement.links.length; ++li) {
-	                        var dasLink = groupElement.links[li];
-	                        link += ' <a href="' + dasLink.uri + '">(' + dasLink.desc + ')</a>';
-	                    }
-	                }
-	            }
-	            
-	           	removeAllPopups();
-	            
-	            var mx =  ev.clientX, my = ev.clientY;
-	            mx +=  document.documentElement.scrollLeft || document.body.scrollLeft;
-	            my +=  document.documentElement.scrollTop || document.body.scrollTop;
-	            var popup = $('#popupTest').clone().css({
-	                position: 'absolute', 
-	                top: (my - 10), 
-	                left:  (mx - 10),
-	                width: 200,
-	                backgroundColor: 'white',
-	                borderColor: 'black',
-	                borderWidth: 1,
-	                borderStyle: 'solid',
-	                padding: 2,
-	            }).html('Gene: ' + label + link).get(0);
-	            $(popup).hide();
-	            hPopupHolder.appendChild(popup);
-	            $(popup).fadeIn(500);
-	            
-	            popup.addEventListener('mouseout', function(ev2) {
-	                    var rel = ev2.relatedTarget;
-	                    while (rel) {
-	                        if (rel == popup) {
-	                            return;
-	                        }
-	                        rel = rel.parentNode;
-	                    }
-	                    removeAllPopups();
-	            }, false);
-		}, true);
-	}
-	
-	blockList.sort(rangeOrder);
-	
-	if (!elideTents) {
-
-        }
-
-	var tier = bump(bumpMatrix, new Range(gmin - 2, gmax + 2));
-	var toffset = tier * renderer.height();
-	g.setAttribute("transform", "translate(0," + toffset + ")");
-	
-	featureGroupElement.appendChild(g);
-	return toffset + renderer.height() + 5;
 }
