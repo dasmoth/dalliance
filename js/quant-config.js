9dfedbbfb009f684d1a32893ebf27f92cd4e3636
Tim Hubbard <th@sanger.ac.uk>
2011-03-27 23:52:41 +0100

    extended quant-config to allow reset of saved values and not fix value unless modified

--- js/quant-config.js
+++ js/quant-config.js
@@ -15,10 +15,23 @@ Browser.prototype.makeQuantConfigButton = function(quantTools, tier, ypos) {
 	ev.stopPropagation(); ev.preventDefault();
 	thisB.removeAllPopups();
 
+        var flagProvideReset=false;
 	var form = makeElement('table');
 	var minInput = makeElement('input', '', {value: tier.min});
+        if(tier.dasSource.forceMin && tier.min == tier.dasSource.forceMin){
+            minInput.style.borderStyle = 'solid';
+            minInput.style.borderColor = 'red';
+            minInput.style.borderWidth = '3px';
+            flagProvideReset=true;
+        }
         form.appendChild(makeElement('tr', [makeElement('td', 'Min:'), makeElement('td', minInput)]));
 	var maxInput = makeElement('input', '', {value: tier.max});
+        if(tier.dasSource.forceMax && tier.max == tier.dasSource.forceMax){
+            maxInput.style.borderStyle = 'solid';
+            maxInput.style.borderColor = 'red';
+            maxInput.style.borderWidth = '3px';
+            flagProvideReset=true;
+        }
         form.appendChild(makeElement('tr', [makeElement('td', 'Max:'), makeElement('td', maxInput)]));
         
 	var updateButton = makeElement('div', 'Update');
@@ -42,13 +55,40 @@ Browser.prototype.makeQuantConfigButton = function(quantTools, tier, ypos) {
                 return;
             }
 
-	    tier.dasSource.forceMin = minInput.value;
-	    tier.dasSource.forceMax = maxInput.value;
+            if(tier.min != minInput.value){
+                tier.dasSource.forceMin = minInput.value;
+            }
+            if(tier.max != maxInput.value){
+                tier.dasSource.forceMax = maxInput.value;
+            }
 	    thisB.removeAllPopups();
             tier.draw();
             thisB.storeStatus();          // write updated limits to storage.
 	}, false);
 
-        thisB.popit(ev, 'Configure: ' + tier.dasSource.name, [form, updateButton]);
+        if(flagProvideReset==false){
+            thisB.popit(ev, 'Configure: ' + tier.dasSource.name, [form, updateButton]);
+        }else{
+            var resetButton = makeElement('div', 'Reset');
+            resetButton.style.backgroundColor = 'rgb(230,230,250)';
+            resetButton.style.borderStyle = 'solid';
+            resetButton.style.borderColor = 'red';
+            resetButton.style.borderWidth = '3px';
+            resetButton.style.padding = '2px';
+            resetButton.style.margin = '10px';
+            resetButton.style.width = '150px';
+
+            resetButton.addEventListener('mousedown', function(ev) {
+                ev.stopPropagation(); ev.preventDefault();
+
+                delete tier.dasSource.forceMin;
+                delete tier.dasSource.forceMax;
+                thisB.removeAllPopups();
+                tier.draw();
+                thisB.storeStatus();          // write updated limits to storage.
+            }, false);
+            thisB.popit(ev, 'Configure: ' + tier.dasSource.name, [form, updateButton, resetButton]);
+        }
+
     }, false);
 }
