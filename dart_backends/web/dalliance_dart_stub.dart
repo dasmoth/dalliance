fb94e9e60e94c9b44e04d5e2760b873281ca914b
Thomas Down <thomas.a.down@googlemail.com>
2013-08-10 22:08:35 +0100

    VCF support.

--- dart_backends/web/dalliance_dart_stub.dart
+++ dart_backends/web/dalliance_dart_stub.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'dart:collection';
 
 import 'package:js/js.dart' as js;
@@ -6,6 +5,7 @@ import 'package:js/js.dart' as js;
 import 'package:bud/io_browser.dart';
 import 'package:bud/tabix.dart';
 import 'package:bud/gff.dart';
+import 'package:bud/vcf.dart';
 
 void main() {
   js.context.createTabixSource = new js.Callback.many(createTabixSource);
@@ -21,6 +21,8 @@ void createTabixSource(String url, String payload, callback) {
     });
 }
 
+
+
 class TabixIndexedSource {
   TabixIndexedFile tif;
   String payload;
@@ -30,10 +32,12 @@ class TabixIndexedSource {
   TabixIndexedSource(this.tif, this.payload) {
     names = new HashSet.from(tif.seqNames);
     
-    if (payload == 'gff2')
-      parser = (String line) => GFFRecord.parse(line, 2);
+    if (payload == 'vcf')
+      parser = dallianceVCF;
+    else if (payload == 'gff2')
+      parser = dallianceGFF2;
     else
-      parser = (String line) => GFFRecord.parse(line, 3);
+      parser = dallianceGFF3;
   }
   
   fetch(String chr, int min, int max, callback) {
@@ -50,36 +54,49 @@ class TabixIndexedSource {
         List records = [];
         
         for (String l in lines) {
-          if (l.startsWith('#')) 
-            continue;
-
-          if (l.length == 0)
-            continue;
-          
-          GFFRecord r = parser(l);
-          
-          var df = {
-            'min': r.start,
-            'max': r.end, 
-            'type': r.type,
-            'source': r.source,
-            'score': r.score
-          };
-          
-          if (r.strand != null) {
-            df['orientation'] = r.strand.token;
-          }
-          
-          if (r.attributes != null && r.attributes.containsKey('transcript_id')) {
-            df['groups'] = [{'id': r.attributes['transcript_id'],
-                          'type': 'gtf-transcript'}];
-          }
-          
-          records.add(js.map(df));
+          records.add(parser(l));
         }
         
         callback(js.array(records));
         js.release(callback);
       });
   }
+}
+
+dallianceGFF2(String s) => dallianceGFF(GFFRecord.parse(s, 2));
+dallianceGFF3(String s) => dallianceGFF(GFFRecord.parse(s, 3));
+
+dallianceGFF(GFFRecord r) {
+  var df = {
+            'min': r.start,
+            'max': r.end, 
+            'type': r.type,
+            'source': r.source,
+            'score': r.score
+   };
+          
+    if (r.strand != null) {
+      df['orientation'] = r.strand.token;
+    }
+    
+    if (r.attributes != null && r.attributes.containsKey('transcript_id')) {
+      df['groups'] = [{'id': r.attributes['transcript_id'],
+                    'type': 'gtf-transcript'}];
+    }
+          
+    return js.map(df);
+}
+
+dallianceVCF(String s) {
+  var r = VCFRecord.parse(s);
+  
+  var df = {
+      'min':    r.pos,
+      'max':    r.pos,
+      'type':   'variant',
+      'score':  r.quality,
+      'id':     r.id
+  };
+  
+  return js.map(df);
 }
