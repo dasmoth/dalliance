b504fb620e9d85afe3fece76bf1a38752a71f40e
Thomas Down <thomas.a.down@googlemail.com>
2013-11-01 17:07:26 +0000

    More options for editing quant tracks.

--- js/tier-edit.js
+++ js/tier-edit.js
@@ -27,6 +27,12 @@ Browser.prototype.openTierPanel = function(tier) {
 
         var tierColorField = makeElement('input', null, {type: 'color', value: '#dd00dd'});
 
+        var tierMinField = makeElement('input', null, {type: 'text', value: '0.0'});
+        var tierMaxField = makeElement('input', null, {type: 'text', value: '10.0'});
+
+        var quantLeapToggle = makeElement('input', null, {type: 'checkbox', checked: tier.quantLeapThreshold !== undefined});
+        var quantLeapThreshField = makeElement('input', null, {type: 'text', value: tier.quantLeapThreshold, disabled: !quantLeapToggle.checked});
+
         if (tier.stylesheet.styles.length > 0) {
             var s = tier.stylesheet.styles[0].style;
             if (s.BGCOLOR) {
@@ -34,6 +40,20 @@ Browser.prototype.openTierPanel = function(tier) {
                 tierColorField.value = dasColourForName(s.BGCOLOR).toHexString();
             }
             glyphField.value = s.glyph;
+
+            if (s.MIN !== undefined) {
+                tierMinField.value = s.MIN;
+            }
+            if (s.MAX !== undefined) {
+                tierMaxField.value = s.MAX;
+            }
+        }
+
+        if (tier.dasSource.forceMin) {
+            tierMinField.value = tier.dasSource.forceMin;
+        }
+        if (tier.dasSource.forceMax) {
+            tierMaxField.value = tier.dasSource.forceMax;
         }
 
         var tierTable = makeElement('table',
@@ -42,12 +62,31 @@ Browser.prototype.openTierPanel = function(tier) {
                  tierNameField]),
 
              makeElement('tr',
-                [makeElement('th', 'Colour (experimental)'),
+                [makeElement('th', 'Colour'),
                  tierColorField]),
 
              makeElement('tr',
                 [makeElement('th', 'Glyph'),
-                 makeElement('td', glyphField)])]);
+                 makeElement('td', glyphField)]),
+
+             makeElement('tr',
+                [makeElement('th', 'Min value:'),
+                 makeElement('td', tierMinField)]),
+
+             makeElement('tr',
+                [makeElement('th', 'Max value:'),
+                 makeElement('td', tierMaxField)]),
+
+             makeElement('tr',
+                [makeElement('th', 'Threshold-leap'),
+                 makeElement('td', quantLeapToggle)]),
+
+             makeElement('tr',
+                [makeElement('th', 'Threshold:'),
+                 makeElement('td', quantLeapThreshField)]),
+             ]);
+
+
         tierForm.appendChild(tierTable);
 
         tierNameField.addEventListener('input', function(ev) {
@@ -65,7 +104,6 @@ Browser.prototype.openTierPanel = function(tier) {
         }
 
         tierColorField.addEventListener('change', function(ev) {
-            console.log(tierColorField.value);
             for (var i = 0; i < tier.stylesheet.styles.length; ++i) {
                 tier.stylesheet.styles[i].style.BGCOLOR = tierColorField.value;
             }
@@ -78,6 +116,33 @@ Browser.prototype.openTierPanel = function(tier) {
             }
             scheduleRedraw();
         }, false);
+
+        tierMinField.addEventListener('input', function(ev) {
+            tier.dasSource.forceMin = parseFloat(tierMinField.value);
+            scheduleRedraw();
+        }, false);
+
+        tierMaxField.addEventListener('input', function(ev) {
+            tier.dasSource.forceMax = parseFloat(tierMaxField.value);
+            scheduleRedraw();
+        }, false);
+
+        function updateQuant() {
+            quantLeapThreshField.disabled = !quantLeapToggle.checked;
+            if (quantLeapToggle.checked) {
+                tier.quantLeapThreshold = parseFloat(quantLeapThreshField.value);
+            } else {
+                tier.quantLeapThreshold = null;
+            }
+            scheduleRedraw();
+        }
+        quantLeapToggle.addEventListener('change', function(ev) {
+            updateQuant();
+        }, false);
+        quantLeapThreshField.addEventListener('input', function(ev) {
+            updateQuant();
+        }, false);
+
 
         this.showToolPanel(tierForm);
         this.setUiMode('tier');
