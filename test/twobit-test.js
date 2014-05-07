d3949e906232832eb1be608f360aea2316263619
Thomas Down <thomas.a.down@googlemail.com>
2014-05-07 13:53:22 +0100

    Tests for big-endian .2bit files.

--- test/twobit-test.js
+++ test/twobit-test.js
@@ -54,9 +54,55 @@ describe('2bit files', function() {
         runs(function() {
             expect(err).toBeFalsy();
             expect(seq).toBeTruthy();
-            console.log(seq.length);
             expect(seq.length).toBe(31);
             expect(seq).toBe('NTCACAGATCACCATACCATNTNNNGNNCNA');
+        });
+    });
+
+    var twoBitURI_be = 'http://www.biodalliance.org/datasets/tests/PGSC_DM.2bit';
+    var twoBit_be;
+
+    it('can be created from a big-endian file', function() {
+        var cb, err;
+        runs(function() {
+             makeTwoBit(new URLFetchable(twoBitURI_be),
+                function(_tb, _err) {
+                    twoBit_be = _tb;
+                    err = _err;
+                    cb = true;
+                });
+       });
+
+        waitsFor(function() {
+            return cb;
+        }, "The callback should be invoked");
+
+        runs(function() {
+            expect(err).toBeFalsy();
+            expect(twoBit_be).not.toBeNull();
+        });
+    });
+
+    it('can retrieve bases from a genomic interval', function() {
+        var seq, err, flag;
+
+        runs(function() {
+            twoBit_be.fetch('chr12', 178140, 178170, function(_s, _e) {
+                flag = true;
+                seq = _s;
+                err = _e;
+            });
+        });
+
+        waitsFor(function() {
+            return flag;
+        }, 'Expects callback after feature fetch');
+
+        runs(function() {
+            expect(err).toBeFalsy();
+            expect(seq).toBeTruthy();
+            expect(seq.length).toBe(31);
+            expect(seq).toBe('CTGTAAGTTGAAGATATTGCATACTTTCTTT');
         });
     });
 });
