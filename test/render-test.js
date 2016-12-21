5f5fe3c766f7a07d875c26a738addb023ac244e4
Thomas Down <thomas.a.down@gmail.com>
2016-12-21 15:10:25 +0000

    Switch to regression-style testing for the renderer.

--- test/render-test.js
+++ test/render-test.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const b = new Browser({
+const renderTestBrowser = new Browser({
     chr:          '11',
     defaultStart: 9000000,
     defaultEnd:   19000000,
@@ -431,7 +431,178 @@ const b = new Browser({
     
     fullScreen: true,
 
-    renderer: 'test'
+    onFirstRender: checkRendering
 });
 
-// b.reset();
+function checkRendering() {
+    fetch('reference-rendering.json')
+        .then(function(resp) { return resp.json(); })
+        .then(checkRendering2)
+        .catch(function(err) {console.log(err)});
+}
+
+function checkRendering2(references) {
+    references = references || {};
+
+    const newReferences = {};
+    const failingTiers = [];
+
+    for (var ti = 0; ti < renderTestBrowser.tiers.length; ++ti) {
+        const tier = renderTestBrowser.tiers[ti];
+        const tierName = tier.dasSource.name;
+
+        console.log('Testing ' + tierName);
+        
+        const tierData = tier.subtiers ? JSON.parse(JSON.stringify(tier.subtiers)) : [];
+        const refData = references[tierName] || [];
+
+        newReferences[tierName] = tierData;
+        
+        if (!compareObjects(tierData, refData)) {
+            failingTiers.push(tierName);
+        }
+    }
+
+    const refBlob = new Blob([JSON.stringify(newReferences, null, 2)]);
+
+    document.body.appendChild(
+        makeElement(
+            'div', 
+            [failingTiers.length == 0 
+                ? makeElement('p', 'All tests passed')
+                : makeElement('p', 'Mismatches for: ' + failingTiers.map(function(n) {return '"' + n + '"'}).join(', ') + " (see console for more details)", {}, {color: 'red'}),
+             makeElement(
+                 'p',
+                 makeElement(
+                     'a', 
+                     'Export reference rendering',
+                     {href: URL.createObjectURL(refBlob),
+                      target: '_new'}
+                 )
+             )]
+        )
+    );
+}
+
+function compareObjects(o1, o2, depth, stack) {
+    depth = depth || 0;
+    stack = stack || [];
+
+    if (typeof(o1) !== typeof(o2)) {
+        printDeep("type mismatch!", depth);
+        printDeep("o1: " + typeof(o1), depth);
+        console.log(o1);
+        printDeep("o2: " + typeof(o2), depth);
+        console.log(o2);
+        printDeep("stack:", depth);
+        prettyStack(stack);
+        console.log("-".repeat(depth));
+        return false;
+    } else {
+        if (o1 === null || o1 === undefined && o1 === o2) {
+            return o1 === o2;
+        }
+
+        if (o1 instanceof Array &&
+            o2 instanceof Array) {
+
+            if (o1.length === o2.length) {
+                var cmp = true;
+                for (var i = 0; i < o1.length; i++) {
+
+                    stack.push({o1: o1[i], o2: o2[i]});
+                    if (!compareObjects(o1[i], o2[i], depth+1, stack)) {
+                        cmp = false;
+                        printDeep("fail on element #" + i, depth);
+                        console.log("-".repeat(depth));
+                        break;
+                    }
+                }
+                return cmp;
+            } else {
+                printDeep("Arrays of different lengths", depth);
+                printDeep("o1: " + typeof(o1), depth);
+                printDeep(o1.length, depth);
+                console.log(o1);
+                printDeep("o2: " + typeof(o2), depth);
+                printDeep(o2.length, depth);
+                console.log(o2);
+                printDeep("stack:", depth);
+                prettyStack(stack);
+                console.log("-".repeat(depth));
+                return false;
+            }
+
+
+        } else if (typeof(o1) === "object" && o1 && o2) {
+            if (Object.keys(o1).length === Object.keys(o2).length ||
+                Object.keys(o1).every(k => k in o2)) {
+
+                var cmp = true;
+                for (var k in o1) {
+                    stack.push({o1: o1[k], o2: o2[k], key: k});
+                    if (!compareObjects(o1[k], o2[k], depth+1, stack)) {
+                        cmp = false;
+                        printDeep("fail when recursing on key " + k, depth);
+                        console.log("-".repeat(depth));
+                        break;
+                    }
+                }
+                return cmp;
+
+            } else {
+                printDeep("Objects have different keys:", depth);
+                printDeep("o1: ", depth);
+                console.log(Object.keys(o1));
+                printDeep("o2: ", depth);
+                console.log(Object.keys(o2));
+                printDeep("stack:", depth);
+                prettyStack(stack);
+                console.log("-".repeat(depth));
+                return false;
+            }
+        } else if (typeof(o1) === 'number' && typeof(o2) === 'number') {
+            if (Math.abs(o1 - o2) <= Math.abs(Math.max(o1, o2)) * 1e-2) {
+                return true;
+            } else {
+                printDeep("fail when comparing numbers", depth);
+                printDeep("primitives not equal: ", depth);
+                printDeep("o1: ", depth);
+                console.log(o1);
+                printDeep("o2: ", depth);
+                console.log(o2);
+                printDeep("stack:", depth);
+                prettyStack(stack);
+                console.log("-".repeat(depth));
+                return false;
+            }
+        } else {
+            if (o1 === o2) {
+                return true;
+            } else {
+                printDeep("fail when comparing primitives", depth);
+                printDeep("primitives not equal: ", depth);
+                printDeep("o1: ", depth);
+                console.log(o1);
+                printDeep("o2: ", depth);
+                console.log(o2);
+                printDeep("stack:", depth);
+                prettyStack(stack);
+                console.log("-".repeat(depth));
+                return false;
+            }
+        }
+    }
+}
+
+function printDeep(str, d) {
+    d = d || 0;
+
+    console.log("/".repeat(d));
+    console.log(str);
+}
+
+
+function prettyStack(stack) {
+    stack.reverse().forEach(function (o, i) {printDeep(o, i)});
+}
